6493e7710d07b9d5c3d12a4f20c8a3a78c769a97
karuna87 <karunasirijakkam@gmail.com>
2016-06-12 14:09:49 +0000

    Javascipt and  COntroller files

---
+++ samplejs/JavaScript1.js
@@ -0,0 +1,14 @@
+﻿
+var app = angular.module('myapp',['ui.grid']);
+    app.controller('MyCtrl', function ($scope, $http) {
+        $http.get("/Home/getGridData")
+        .then(function (response) {
+            debugger;
+            $scope.myData = [{ empid: response.data[0].empid, empname: response.data[0].empname, empage: response.data[0].empage }]
+            debugger;
+            $scope.gridOptions = { data: 'myData' };
+        }, function () {
+            debugger;
+            alert("No Data found");
+        });
+    });
